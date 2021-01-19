98793cff6d9048814cf4558f377bc805d5ea8917
Junhong-Kim <jhk1005jhk@gmail.com>
2021-01-19 22:47:35 +0900

    init

---
+++ src/test/java/kim/junhong/expspringbatch/BatchApplicationTests.java
@@ -0,0 +1,13 @@
+package kim.junhong.expspringbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
